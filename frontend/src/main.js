7e4722b8f7b762da54d8fabce2914021eb2d850c
Goki Kanno <goki.kanno@gmail.com>
2024-07-14 20:09:48 +0900

    コメント追加

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,13 +12,16 @@ const pinia = createPinia();
 const app = createApp(App)
 // Cookieからトークンを読み込んで設定
 const getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    const value = `; ${document.cookie}`;  // 全てのCookieを文字列として取得し、前にセミコロンとスペースを追加
+    const parts = value.split(`; ${name}=`);  // 指定された名前のCookieを探すために分割
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();  // Cookieの値を取得して返す
+    }
 };
+// Cookieから'token'という名前の値を取得
 const token = getCookie('token');
 if (token) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;  // トークンが存在する場合、'Authorization'ヘッダーに設定
 }
 
 app.use(router)
