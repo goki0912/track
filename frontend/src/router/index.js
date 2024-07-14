84cada829114927d40844b64d8c806213e4be1e7
Goki Kanno <goki.kanno@gmail.com>
2024-07-14 19:49:30 +0900

    ログイン機能完成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import UserAuth from '../components/UserAuth.vue'
-import HomePage from '../components/HomePage.vue' // ホームページ用のコンポーネントを作成します
+import HomePage from '../components/HomePage.vue'
+import LoginPage from "@/components/LoginPage.vue";// ホームページ用のコンポーネントを作成します
+import RegisterPage from "@/components/RegisterPage.vue";
+import { useAuthStore } from '@/stores/auth';
+
 
 const routes = [
-    { path: '/', component: HomePage },
-    { path: '/auth', component: UserAuth }
+    {
+        path: '/login',
+        component: LoginPage,
+        meta: {
+            requiresAuth: false,
+            guestOnly: true
+        }
+    },
+    {
+        path: "/register",
+        component: RegisterPage,
+        meta: {
+            requiresAuth: false,
+            guestOnly: true
+        }
+    },
+    {
+        path: '/home',
+        component: HomePage,
+        meta: {
+            requiresAuth: true
+        }
+    },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+    authStore.checkAuth(); // ここで認証状態を確認
+    if (to.matched.some(record => record.meta.requiresAuth) && !authStore.isAuthenticated) {
+        next('/login');
+    } else if (to.matched.some(record => record.meta.guestOnly) && authStore.isAuthenticated) {
+        next('/home');
+    } else {
+        next();
+    }
+});
 
 export default router
