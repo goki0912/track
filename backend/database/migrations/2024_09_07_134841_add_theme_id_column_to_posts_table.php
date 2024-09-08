44220070035ec4c6467cdc552517aa7d0e68720f
Goki Kanno <goki.kanno@gmail.com>
2024-09-09 01:35:00 +0900

    add:theme table

---
+++ backend/database/migrations/2024_09_07_134841_add_theme_id_column_to_posts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+//            nullable後で外す
+            $table->foreignId('theme_id')->after('track_id')->constrained('themes');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('theme_id');
+        });
+    }
+};
