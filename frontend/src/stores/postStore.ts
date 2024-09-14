5635bd60f9a84fc239728ec1a1148f64047ef6be
Goki Kanno <goki.kanno@gmail.com>
2024-09-14 17:45:55 +0900

    fix:lint

--- frontend/src/stores/postStore.ts
+++ frontend/src/stores/postStore.ts
@@ -8,18 +8,18 @@ export const usePostStore = defineStore("post", {
     likedPosts: [] as number[],
   }),
   actions: {
-    async fetchPosts(theme_id: number) {
+    async fetchPosts(themeId: number) {
       try {
-        const response = await axios.get(`spotify/theme/${theme_id}/posts`);
+        const response = await axios.get(`spotify/theme/${themeId}/posts`);
         this.posts = response.data.posts;
         this.likedPosts = response.data.likedPosts;
       } catch (error) {
         console.error("Failed to fetch posts", error);
       }
     },
-    async createPost(theme_id: number, track: Track) {
+    async createPost(themeId: number, track: Track) {
       try {
-        await axios.post(`spotify/theme/${theme_id}/posts`, track);
+        await axios.post(`spotify/theme/${themeId}/posts`, track);
         await this.fetchPosts(theme_id);
       } catch (error) {
         console.error("Failed to create a post", error);
